ef85b5a7f38a7d2d62598e279c83b8866aa0612b
qinpmc <850823973@qq.com>
2019-03-12 22:43:52 +0800

    add notes

---
+++ demo2-1/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  // JS 执行入口文件---多个入口
+  entry: {
+    bundle1: './main1.js',
+    bundle2: './main2.js'
+  },
+  output: {
+    // 把所有依赖的模块合并输出到一个 bundle.js 文件
+    filename: '[name].js',
+    // 输出文件都放到 dist 目录下
+    path: path.resolve(__dirname, './dist'),
+  } 
+};
