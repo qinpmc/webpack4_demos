3cbf3f15dc1e429a4498a8ebcbae35687b91ac4d
qinpmc <850823973@qq.com>
2019-03-21 21:29:06 +0800

    Add less

---
+++ demo7_less/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  // JS 执行入口文件
+  entry: './main.js',
+  output: {
+    // 把所有依赖的模块合并输出到一个 bundle.js 文件
+    filename: 'bundle.js',
+    // 输出文件都放到 dist 目录下
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+		test: /\.less$/,// 增加对 less文件的支持
+		use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+	  {
+		  test: /\.css$/,
+		  use:[
+		  {
+			loader: 'style-loader',
+			options:{
+				insertAt:'top'
+			}   
+		  },
+		  'css-loader'  
+		  ]
+	  }
+    ]
+  },
+};
